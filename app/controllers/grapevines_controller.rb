f33713c47ba23609b845d5bd61021843aa9befd3
Martin Boulland <martinboulland@gmail.com>
2023-05-31 13:00:23 +0200

    ajout de photo dans la création de profil

--- app/controllers/grapevines_controller.rb
+++ app/controllers/grapevines_controller.rb
@@ -15,6 +15,12 @@ class GrapevinesController < ApplicationController
   def create
     @grapevine = Grapevine.new(params_grapevine)
     @grapevine.user = current_user
+    if @grapevine.photo.blank?
+      # Définir la photo par défaut
+      default_photo_path = Rails.root.join('app', 'assets', 'images', 'default-image-grappe.jpeg')
+      @grapevine.photo.attach(io: File.open(default_photo_path), filename: 'default-image-grappe.jpeg', content_type: 'image/jpg')
+    end
+
     if @grapevine.save
 
       redirect_to grapevine_path(@grapevine)
@@ -29,7 +35,7 @@ class GrapevinesController < ApplicationController
   private
 
   def params_grapevine
-    params.require(:grapevine).permit(:grape_variety, :organic, :area, :parcel_price, :parcel_stock)
+    params.require(:grapevine).permit(:grape_variety, :organic, :area, :parcel_price, :parcel_stock, :photo)
   end
 
 end
