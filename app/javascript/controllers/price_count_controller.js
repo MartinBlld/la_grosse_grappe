4167e851f32ca9b10e61559db2a04bd4224a4c9e
Martin Boulland <martinboulland@gmail.com>
2023-06-01 11:57:26 +0200

    Le counter en direct

--- app/javascript/controllers/price_count_controller.js
+++ app/javascript/controllers/price_count_controller.js
@@ -4,29 +4,59 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="price-count"
 export default class extends Controller {
 
-  static targets = ["counter"];
+  static targets = ["parcelQuantityInput", "yearsInput", "priceHolder"];
+  static values = { price: Number }
 
 
-  updateYearCounter(event) {
-   const YearCounter = event.target.value;
-    console.log(YearCounter)
-  }
+  updatePrice() {
 
-  updateStockCounter(event){
-    const StockCounter = event.target.value;
-    console.log(StockCounter)
-    return
+    const years = this.yearsInputTarget.value
+    const parcelQuantity = this.parcelQuantityInputTarget.value
+    const totalValue = this.#totalValue(years, parcelQuantity)
+
+    this.#setPriceValue(totalValue)
     }
 
-  updatePriceCounter(event){
-    // if (condition) {
+    #setPriceValue(value) {
 
-    // } else {
+    this.priceHolderTarget.innerText = value
+    }
 
-    // }
-  YearCounter = this.updateYearCounter(event)
-  StockCounter = this.updateStockCounter(event)
-  this.counterTarget.innerHTML('<p> <%= ${this.StockCounter * this.YearCounter} * grapveine.parcel_price %> </p>')
+    #totalValue(years, parcelQuantity) {
+      if (years == 0) {
+        return (1)*(parcelQuantity) * this.priceValue
+      } else if (parcelQuantity == 0)
+        return (years)*(1) * this.priceValue
+      else {
+    return (years)*(parcelQuantity) * this.priceValue
+    }
+  }
+
 }
 
-  }
+
+
+
+
+
+//   updateYearCounter(event) {
+//    const YearCounter = event.target.value;
+//     console.log()
+//   }
+
+//   updateStockCounter(event){
+//     const StockCounter = event.target.value;
+//     console.log(StockCounter)
+//     return
+//     }
+
+//   updatePriceCounter(event){
+//     // if (condition) {
+
+//     // } else {
+
+//     // }
+//   YearCounter = this.updateYearCounter(event)
+//   StockCounter = this.updateStockCounter(event)
+//   this.counterTarget.innerHTML('<p> <%= ${this.StockCounter * this.YearCounter} * grapveine.parcel_price %> </p>')
+// }
